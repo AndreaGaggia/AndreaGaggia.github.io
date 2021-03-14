0a9a762d6bea63ec3febac7aa4b5a999804861d4
AndreaGaggia <gaggiandrea@gmail.com>
2021-03-14 12:42:12 +0100

    embed video test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="home">
         <h1>Ciao</h1>
+        <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/lVQMyoio598?controls=0&autoplay=1&loop=1"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        ></iframe>
     </div>
 </template>
 
@@ -11,4 +19,9 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+iframe {
+    border-radius: 8px;
+    box-shadow: 8px 8px crimson;
+}
+</style>
